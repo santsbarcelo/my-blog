6ddbad87f7398ef8ac38e6e8daf4841997841e21
{santsbarcelo} <{62164626+santsbarcelo@users.noreply.github.com}>
2022-03-28 17:42:17 +0900

    ✨トップページにサムネイル画像とタグを追加しました

--- pages/index.js
+++ pages/index.js
@@ -4,28 +4,38 @@ import { client } from "../libs/client";
 
 export default function Home({ blog, totalCount }) {
   return (
-    
-    <div>
+    <>
+      <h1 className="container mx-auto px-10 pt-10 grid grid-cols-1 sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3">
+        記事一覧：{totalCount}件
+      </h1>
       <div>
-      件数：{totalCount}件
-    </div>
-      <ul>
-        {blog.map((blog) => (
-          <li key={blog.id}>
-            <Link href={`/blog/${blog.id}`}>
-              <a>{blog.title}:{blog.createdAt}</a>
-            </Link>
-          </li>
-        ))}
-      </ul>
-    </div>
+        <ul className="container mx-auto px-10 pt-10 grid grid-cols-1 sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3 gap-5">
+          {blog.map((blog) => (
+            <li key={blog.id} className="rounded overflow-hidden shadow-lg">
+              <Link href={`/blog/${blog.id}`}>
+                <a>
+                  <img src={blog.image.url} alt={blog.image.name} />
+                  <div>
+                    <h2 className="px-6 pt-4">{blog.title}</h2>
+                    <div className="px-6 pb-2">{blog.createdAt}</div>
+                    <div className="px-6 pb-4">
+                      <span># {blog.tag}</span>
+                    </div>
+                  </div>
+                </a>
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </>
   );
 }
 
 // データをテンプレートに受け渡す部分の処理を記述します
 export const getStaticProps = async () => {
   const data = await client.get({ endpoint: "blog" });
-console.log(data);
+  console.log(data);
   return {
     props: {
       blog: data.contents,
